7f0e1fc973588708d1d1d65cfbebd646a1581bbd
Karthik-56 <karthiksurya611@gmail.com>
2024-09-22 18:10:31 +0530

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# this file is used to create local folders/files as local package
+from setuptools import find_packages,setup
+
+setup(
+  name='Interview-Assistant',
+  version='0.0.0',
+  url="https://github.com/KaRtHiK-56/EDA_python_package_library",  
+  author='Karthik',
+  author_email='karthiksurya611@gmail.com',
+  packages = find_packages(),
+  install_requires = []
+)
